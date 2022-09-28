40681846a61750943cc58782e5889d44ea4aac37
Ryan <ryansoto361@hotmail.com>
2022-09-28 16:00:21 -0500

    Sign up working

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Login Project</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    include_once 'header.php';
+?>
+
+            <section class="index-into">
+                <h1>This Is An Introduction</h1>
+                <p>Here is an important paragraph that explains the purpose of the website</p>
+            </section>
+            <section class="index-categories">
+                <h2>Some Basic Categories</h2>
+                <div class="index-categories-list">
+                    <div>
+                        <h3>Fun STuff</h3>
+                    </div>
+                    <div>
+                        <h3>Serious Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Serious Stuff</h3>
+                    </div>
+                    <div>
+                        <h3>Boring Stuff</h3>
+                    </div>
+                </div>
+            </section>
+        
+<?php
+    include_once 'footer.php';
+?>
